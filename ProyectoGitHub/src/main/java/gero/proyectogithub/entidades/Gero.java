344a74d64b5de2472ad728291753d69c3ba13778
GeroKramar <gerokramar@gmail.com>
2023-10-02 23:19:59 -0300

    clase

--- ProyectoGitHub/src/main/java/gero/proyectogithub/entidades/Gero.java
+++ ProyectoGitHub/src/main/java/gero/proyectogithub/entidades/Gero.java
@@ -1,11 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 package gero.proyectogithub.entidades;
 
 
 public class Gero {
-private S
+private String nombre;
+private String apellido;
+
+    public Gero() {
+    }
+
+    public Gero(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
 }
