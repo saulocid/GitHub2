3ea21085868ca659c9d611a8b832c4232b1db8c9
GeroKramar <gerokramar@gmail.com>
2023-10-02 23:15:14 -0300

    crear proyecto

---
+++ ProyectoGitHub/src/main/java/gero/proyectogithub/ProyectoGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gero.proyectogithub;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProyectoGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
